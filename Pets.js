0ba643e806d5194ccc20603db2ea3aac4ca98393
KuandykBadanov <118505559+KuandykBadanov@users.noreply.github.com>
2022-12-08 08:24:41 +0600

    Update Pets.js

--- Pets.js
+++ Pets.js
@@ -1,30 +1,45 @@
-class Pets {
-
-    constructor(options) {
-       this.name = options.name
-       this.speak = options.speak
-       this.move = options.move
-       this.owner = options.owner
-       this.belongsTo = options.belongsTo
+class Animal {
+    constructor(name, owner, voice) {
+      this.name = name;
+      this.owner = owner;
+      this.voice = voice;
     }
-
-}
-
-class Dog extends Pets {
-     const dog = new Dog({
-        name: 'Rex',
-        speak: 'Gaf Gaf',
-        move: 'steps',
-        owner: 'Ranger'
-    })
-}
-
-class Cat extends Pets {
-    const cat = new Cat({
-        name: 'Pussy',
-        speak: 'Meow Meow',
-        move: 'steps',
-        owner: 'Girl'
+  
+    speak() {
+      console.log(`${this.name} says ${this.voice}`);
+    }
+  
+    move(stepsNum) {
+      console.log(`${this.name} moves by ${stepsNum} step`);
+    }
+  
+    belongsTo() {
+      console.log(`${this.name} belongs to ${this.owner}`);
+    }
+  }
+  
+  class Cat extends Animal {
+    constructor(name, owner, voice) {
+      super(name, owner, voice);
+    }
     
-    })
-}
+    
+  }
+  
+  class Dog extends Animal {
+    constructor(name, owner, voice) {
+      super(name, owner, voice);
+    }
+    
+    
+  }
+  
+  const garfield = new Cat("Garfield", "John", "I hate Mondays");
+  const scoobydoo = new Dog("Scooby-Doo", "Shaggy", "Scooby-Doo Scooby-Doo");
+  
+  garfield.speak();
+  garfield.move(3);
+  garfield.belongsTo();
+  scoobydoo.speak();
+  scoobydoo.move(4);
+  scoobydoo.belongsTo();
